f0a673dbebe0d58e0b0186cb8538bf3f4c81b8a8
ATVI\setienne <setienne@highmoonstudios.com>
2025-08-25 23:18:05 -0700

    Added catch2 and verified it all compiles and runs

--- tests/math_tests.cpp
+++ tests/math_tests.cpp
@@ -1,8 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 import engine.math;
+
+using Catch::Matchers::WithinRel;
 TEST_CASE("Dot and cross", "[math]"){
     math::Vec3 x{1,0,0}, y{0,1,0};
-    REQUIRE(math::dot(x,x)==Approx(1));
-    auto c=math::cross(x,y);
-    REQUIRE(c.z==Approx(1));
+    REQUIRE_THAT(math::dot(x,x), WithinRel(1.0f));
+    auto c = math::cross(x,y);
+    REQUIRE_THAT(c.z, WithinRel(1.0f));
 }
